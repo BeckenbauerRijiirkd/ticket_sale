56cec6e4acd696afe429639b89204102bfdaa211
beckenbauer <you@example.com>
2021-12-22 15:32:07 -0300

    criar evento - init

--- src/main/java/ticket/sale/ticket_sale/controller/EventoController.java
+++ src/main/java/ticket/sale/ticket_sale/controller/EventoController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ticket.sale.ticket_sale.controller.dto.EventoDto;
+import ticket.sale.ticket_sale.controller.modelViewer.EventoModelViewer;
 import ticket.sale.ticket_sale.model.Evento;
 import ticket.sale.ticket_sale.repository.EventoRepository;
 import ticket.sale.ticket_sale.repository.VendaRepository;
@@ -35,6 +38,12 @@ public class EventoController {
 
         return ResponseEntity.ok(EventoDto.converter(eventos));
     
+    }
+
+    @PostMapping
+    public void CriarEvento(@RequestBody EventoModelViewer eventoModelViewer){
+
+        System.out.println(eventoModelViewer);
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,8 @@ INSERT INTO RESPONSAVEL(nome, nascimento, cpf, cidade, uf, endereco) VALUES('Res
 
 INSERT INTO EVENTO(nome, data, responsavel_id, valor, valor_meia, status) VALUES('Evento Teste', '2020-10-20', '1', '20', '10', 'REALIZADO');
 
+INSERT INTO EVENTO(nome, data, responsavel_id, valor, valor_meia, status) VALUES('Evento Teste 2', '2020-10-22', '1', '20', '10', 'REALIZADO');
+
 INSERT INTO CLIENTE(nome, data_nasc, cpf, cidade, uf) VALUES('Cliente teste', '1990-11-21', '7070707070', 'aracaju', 'SE');
 
 INSERT INTO VENDA(evento_id, cliente_id, tipo_ingresso, qtd_ingresso, valor_total) VALUES('1', '1', 'INTEIRA', '2','40');
