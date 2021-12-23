cf6cbb142094de358760f4876a20207e9e3bb0f4
beckenbauer <you@example.com>
2021-12-23 09:33:32 -0300

    GetVendas

---
+++ src/main/java/ticket/sale/ticket_sale/controller/VendaController.java
@@ -0,0 +1,30 @@
+package ticket.sale.ticket_sale.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ticket.sale.ticket_sale.controller.dto.VendaDto;
+import ticket.sale.ticket_sale.model.Venda;
+import ticket.sale.ticket_sale.repository.VendaRepository;
+
+@RestController
+@RequestMapping("/vendas")
+public class VendaController {
+    
+    @Autowired
+    private VendaRepository vendaRepository;
+    
+    @GetMapping
+    public ResponseEntity<List<VendaDto>> listar(){
+        
+        List<Venda> vendas = vendaRepository.findAll();
+
+        return ResponseEntity.ok(VendaDto.converter(vendas));
+    
+    }
+}
---
+++ src/main/java/ticket/sale/ticket_sale/controller/dto/VendaDto.java
@@ -0,0 +1,81 @@
+package ticket.sale.ticket_sale.controller.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ticket.sale.ticket_sale.model.Venda;
+
+public class VendaDto {
+    private Long id;
+    private String evento;
+    private String cliente;
+    private String tipoDoIngresso;
+    private int ingressos;
+    private Double valorTotal;
+
+    public VendaDto() {
+    }
+
+    public VendaDto(Venda venda) {
+        this.id = venda.getId();
+        this.evento = venda.getEvento().getNome();
+        this.cliente = venda.getCliente().getNome();
+        this.tipoDoIngresso = venda.getTipoIngresso().name();
+        this.ingressos = venda.getQtdIngresso();
+        this.valorTotal = venda.getValorTotal();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getTipoDoIngresso() {
+        return tipoDoIngresso;
+    }
+
+    public void setTipoDoIngresso(String tipoDoIngresso) {
+        this.tipoDoIngresso = tipoDoIngresso;
+    }
+
+    public int getIngressos() {
+        return ingressos;
+    }
+
+    public void setIngressos(int ingressos) {
+        this.ingressos = ingressos;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public static List<VendaDto> converter(List<Venda> vendas){
+        return (vendas.stream().map(VendaDto::new).collect(Collectors.toList()));
+    }
+    
+    
+}
