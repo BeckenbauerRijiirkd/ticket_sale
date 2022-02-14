95e2f4618b888822c92b9033bc6b677b760a969d
Beck Raika <beckraika@gmail.com>
2022-02-14 12:50:24 -0300

    teste

--- src/main/java/ticket/sale/ticket_sale/controller/ClienteController.java
+++ src/main/java/ticket/sale/ticket_sale/controller/ClienteController.java
@@ -47,7 +47,7 @@ public class ClienteController {
     @PostMapping
     @Transactional
     public ResponseEntity<String> cadastrarCliente(@RequestBody @Valid ClienteModelViewer clienteModelViewer) {
-//teste-deploy-aut
+//teste-deploy
         if (usuarioService.verificarEmail(clienteModelViewer.getEmail())) {
             return ResponseEntity.badRequest().body("Email Ja Cadastrado");
         }
